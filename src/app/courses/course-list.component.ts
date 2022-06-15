89ab6c3741e23b7b4d8614d4162ea00f2a94fb68
MarcusVLB <vinimarcus02@gmail.com>
2022-06-15 16:35:33 -0300

    First Commit

---
+++ src/app/courses/course-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { Course } from "./course";
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list-component.html'
+})
+export class CourseListComponent{
+
+    courses: Course[] = [];
+    
+
+}
