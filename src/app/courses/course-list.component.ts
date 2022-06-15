72297a10091df7ce4ea51a6aa8abdf1527465a9e
MarcusVLB <vinimarcus02@gmail.com>
2022-06-15 17:09:48 -0300

    Commit Inicial Complementar

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,13 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Course } from "./course";
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list-component.html'
 })
-export class CourseListComponent{
+export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
     
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Curso de Pulinho',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XRAY-365',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'November, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Curso de Eject',
+                imageUrl: '',
+                price: 45.99,
+                code: 'XRAY-322',
+                duration: 80,
+                rating: 5,
+                releaseDate: 'December, 4, 2019'
+            }
+        ]
+    }
 
 }
