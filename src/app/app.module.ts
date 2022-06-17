82bd7e85ee30f360938382db0bfd05b622521251
MarcusVLB <vinimarcus02@gmail.com>
2022-06-17 09:31:04 -0300

    Segregando Responsabilidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,45 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 
-import { CourseListComponent } from './courses/course-list.component';
-import { StarComponent } from './star/star.component';
-import { ReplacePipe } from './pipe/replace.pipe';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { Error404Component } from './error-404/error-404.component';
-import { CourseInfoComponent } from './courses/course-info.component';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
-    NavBarComponent,
-    Error404Component,
-    CourseInfoComponent
+    AppComponent    
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([     
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },      
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
